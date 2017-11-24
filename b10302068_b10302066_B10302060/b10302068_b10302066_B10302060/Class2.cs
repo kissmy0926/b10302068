dd65d458ec6d3dae9dfdf03f25620ec29b2600df
ggyy00965814 <33939755+ggyy00965814@users.noreply.github.com>
2017-11-24 21:12:57 +0800

    program3

--- b10302068_b10302066_B10302060/b10302068_b10302066_B10302060/Class2.cs
+++ b10302068_b10302066_B10302060/b10302068_b10302066_B10302060/Class2.cs
@@ -10,6 +10,22 @@ namespace b10302068_b10302066_B10302060
     {
         public bool IsPrime(int n)
         {
+
+            if (n < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < n; ++i)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
             throw new NotImplementedException();
         }
     }

9684d7df184c750b348f9962af0d31933d205a50
kissmy0926 <33798138+kissmy0926@users.noreply.github.com>
2017-11-24 18:02:17 +0800

    program1

---
+++ B10302068_b10302060_b10302066/B10302068_b10302060_b10302066/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B10302068_b10302060_b10302066
+{
+    class Class2
+    {
+        internal bool IsPrime(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
